82f6e783d09187bf2d972911fdae7f558a07d856
misial97 <35803878+misial97@users.noreply.github.com>
2019-09-30 18:54:33 +0200

    Initial commit - "Colas" EDA 19/20

---
+++ src/material/linear/ArrayQueue.java
@@ -0,0 +1,28 @@
+package material.linear;
+
+public class ArrayQueue<E> implements Queue<E> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E front() {
+        return null;
+    }
+
+    @Override
+    public void enqueue(E element) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+}
---
+++ src/material/linear/LinkedQueue.java
@@ -0,0 +1,28 @@
+package material.linear;
+
+public class LinkedQueue<E> implements Queue<E> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E front() {
+        return null;
+    }
+
+    @Override
+    public void enqueue(E element) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+}
